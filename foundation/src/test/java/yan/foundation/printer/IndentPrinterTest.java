f9df2b693b0df9b83ab3d8769f6482753d8d9191
Zeqiang-Lai <916323301@qq.com>
2020-03-08 10:45:10 +0800

    test: add test cases for IndentPrinter

---
+++ foundation/src/test/java/yan/foundation/printer/IndentPrinterTest.java
@@ -0,0 +1,33 @@
+package yan.foundation.printer;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IndentPrinterTest {
+    ClassLoader classLoader = getClass().getClassLoader();
+
+    @Test
+    public void testBasic() {
+        IndentPrinter printer = new IndentPrinter(4);
+        printer.println("Token");
+        printer.indent();
+        printer.println("text: +").println("type: ADD").println("line: 1").println("column: 2");
+        printer.print("index:").print(" ").formatln("%d", 4);
+        printer.unindent();
+        printer.print("/Token");
+        String out = printer.flush();
+
+        try (InputStream inputStream = classLoader.getResourceAsStream("printer/IdentPrinterTestCase1.txt")) {
+            String result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+            assertEquals(out, result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
