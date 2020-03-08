361b87c40508f22bf9130e6d3406a4c962d8e4f1
Zeqiang-Lai <916323301@qq.com>
2020-03-08 11:02:22 +0800

    feat: complete the implementation of XMLPrinter

--- foundation/src/main/java/yan/foundation/printer/XMLPrinter.java
+++ foundation/src/main/java/yan/foundation/printer/XMLPrinter.java
@@ -1,7 +1,7 @@
 package yan.foundation.printer;
 
 import java.io.PrintWriter;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * A Simple XML Printer for printing xml content.
@@ -27,10 +27,14 @@ public class XMLPrinter {
     // ================ Public API ================ //
 
     private IndentPrinter printer = new IndentPrinter(4);
-    private Stack<String> elements = new Stack<>();
+    private Stack<Element> elements = new Stack<>();
+
+    public XMLPrinter(String root, boolean compact_mode) {
+        openElement(root, compact_mode);
+    }
 
     public XMLPrinter(String root) {
-        openElement(root);
+        openElement(root, false);
     }
 
     public void openElement(String name) {
@@ -38,45 +42,116 @@ public class XMLPrinter {
     }
 
     public void openElement(String name, boolean compact_mode) {
-        elements.push(name);
-        printer.format("<%s>", name);
-        if (!compact_mode) printer.println();
+        elements.push(new Element(name, compact_mode));
+        elements.peek();
     }
 
-    public void closeElement(boolean compact_mode) {
-        elements.pop();
+    public void closeElement() {
+        Element element = elements.pop();
+        elements.peek().addChild(element);
     }
 
     public void pushAttribute(String name, String value) {
-
+        elements.peek().addAttribute(name, value);
     }
 
     public void pushText(String text) {
-
+        elements.peek().setText(escape(text));
     }
 
-    public void pushComment(String text) {
-        printer.println(String.format("<!-- %s --> ", text));
+    public void pushSimpleElement(String name, String text) {
+        openElement(name, true);
+        pushText(text);
+        closeElement();
     }
-
-    // =================== Implementation =================== //
 
     public String flush() {
-        // TODO: validate
-        return printer.flush();
+        // Simple validation
+        if (elements.size() > 1)
+            throw new RuntimeException(String.format("You have unclosed elements <%s>. Please check your code.",
+                    elements.peek().name));
+        return elements.peek().toString(printer);
     }
 
     public void flush(PrintWriter out) {
         out.print(flush());
     }
 
-    private String escape(String text) {
+    public String escape(String text) {
         text = text.replace("&", "&amp;");
         text = text.replace("<", "&lt;");
         text = text.replace(">", "&gt;");
         text = text.replace("'", "&apos;");
         text = text.replace("\"", "&quot;");
         return text;
+    }
+
+    public static class Element {
+        String name;
+        String text;
+        Map<String, String> attributes = new HashMap<>();
+        List<Element> children = new ArrayList<>();
+
+        boolean isCompactMode;
+
+        public Element(String name, boolean isCompactMode) {
+            this.name = name;
+            this.isCompactMode = isCompactMode;
+        }
+
+        public void addAttribute(String name, String value) {
+            attributes.put(name, value);
+        }
+
+        public void addChild(Element child) {
+            children.add(child);
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
+        public String toString(IndentPrinter printer) {
+            /*
+             <Note>
+                Test
+                <from>apple</from>
+             <Note>
+
+             start_compact:
+                <Note>
+                    apple</Note>
+             */
+
+            // Print start tag: <name key="value">
+            StringBuilder attribute_str = new StringBuilder();
+            attributes.forEach((key, value) -> attribute_str.append(String.format(" %s=\"%s\"", key, value)));
+            printer.format("<%s%s>", name, attribute_str.toString());
+            if (!isCompactMode) {
+                printer.println();
+                printer.indent();
+            }
+
+            // Print text
+            if (text != null) {
+                printer.print(text);
+                if (!isCompactMode) printer.println();
+            }
+
+            // Print children
+            for (var child : children) {
+                child.toString(printer);
+                if (!isCompactMode) printer.println();
+            }
+
+            // Print end tag: </name>
+            if (!isCompactMode) {
+                printer.unindent();
+            }
+            printer.format("</%s>", name);
+
+            return printer.flush();
+        }
     }
 
 }
---
+++ foundation/src/test/java/yan/foundation/printer/XMLPrinterTest.java
@@ -0,0 +1,80 @@
+package yan.foundation.printer;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class XMLPrinterTest {
+    private final XMLPrinter printer = new XMLPrinter("note");
+    ClassLoader classLoader = getClass().getClassLoader();
+
+    @Test
+    public void testEscape() {
+        String out = printer.escape("<test>");
+        assertEquals("&lt;test&gt;", out);
+    }
+
+    @Test
+    public void testException() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            printer.openElement("test");
+            String out = printer.flush();
+        });
+    }
+
+    @Test
+    public void testBasic() {
+        printer.openElement("text", true);
+        printer.pushText("+");
+        printer.closeElement();
+        printer.pushSimpleElement("type", "ADD");
+        printer.pushSimpleElement("line", "1");
+        printer.pushSimpleElement("Column", "2");
+
+        String out = printer.flush();
+        try (InputStream inputStream = classLoader.getResourceAsStream("printer/XMLPrinterTestCase1.xml")) {
+            String result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+            assertEquals(result, out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testNest() {
+        var printer = new XMLPrinter("program");
+        printer.openElement("function");
+        printer.pushSimpleElement("name", "add");
+
+        printer.openElement("params");
+
+        printer.openElement("param");
+        printer.pushSimpleElement("name", "x");
+        printer.pushSimpleElement("type", "int");
+        printer.closeElement();
+
+        printer.openElement("param");
+        printer.pushSimpleElement("name", "y");
+        printer.pushSimpleElement("type", "int");
+        printer.closeElement();
+
+        printer.closeElement();
+
+        printer.closeElement();
+
+        String out = printer.flush();
+        try (InputStream inputStream = classLoader.getResourceAsStream("printer/XMLPrinterTestCase2.xml")) {
+            String result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+            assertEquals(result, out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
