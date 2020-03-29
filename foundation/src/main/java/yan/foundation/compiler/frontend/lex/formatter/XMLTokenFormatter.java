ffbc58fdea3f17db2066760c4b8c230af9a276bb
Zeqiang-Lai <916323301@qq.com>
2020-03-29 16:13:39 +0800

    refactor: change xml element type to lowercase (lexer output)

--- foundation/src/main/java/yan/foundation/compiler/frontend/lex/formatter/XMLTokenFormatter.java
+++ foundation/src/main/java/yan/foundation/compiler/frontend/lex/formatter/XMLTokenFormatter.java
@@ -8,17 +8,17 @@ import java.util.List;
 public class XMLTokenFormatter extends TokenFormatter {
     @Override
     public String toString(List<Token> tokens) {
-        XMLPrinter printer = new XMLPrinter("Tokens");
+        XMLPrinter printer = new XMLPrinter("tokens");
         for (Token token : tokens) {
-            printer.openElement("Token");
-            printer.pushSimpleElement("Type", token.getTypeString());
-            printer.pushSimpleElement("Source", token.source.getSourceName());
-            printer.pushSimpleElement("Lexer", token.lexer.getClass().getSimpleName());
-            printer.pushSimpleElement("Value", String.valueOf(token.value));
-            printer.pushSimpleElement("Line", String.valueOf(token.line));
-            printer.pushSimpleElement("Column", String.valueOf(token.col));
-            printer.pushSimpleElement("Start", String.valueOf(token.start));
-            printer.pushSimpleElement("Stop", String.valueOf(token.stop));
+            printer.openElement("token");
+            printer.pushSimpleElement("type", token.getTypeString());
+            printer.pushSimpleElement("source", token.source.getSourceName());
+            printer.pushSimpleElement("lexer", token.lexer.getClass().getSimpleName());
+            printer.pushSimpleElement("value", String.valueOf(token.value));
+            printer.pushSimpleElement("line", String.valueOf(token.line));
+            printer.pushSimpleElement("column", String.valueOf(token.col));
+            printer.pushSimpleElement("start", String.valueOf(token.start));
+            printer.pushSimpleElement("stop", String.valueOf(token.stop));
             printer.closeElement();
         }
         return printer.flush();
