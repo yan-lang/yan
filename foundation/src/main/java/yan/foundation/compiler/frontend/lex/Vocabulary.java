0285e0a037b43b0a49972f0327dd9627e1d37f22
Zeqiang-Lai <916323301@qq.com>
2020-04-15 09:14:39 +0800

    docs: add docs

--- foundation/src/main/java/yan/foundation/compiler/frontend/lex/Vocabulary.java
+++ foundation/src/main/java/yan/foundation/compiler/frontend/lex/Vocabulary.java
@@ -3,6 +3,10 @@ package yan.foundation.compiler.frontend.lex;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * {@link Token#type}使用int类型, 在print Token的时候，我们需要将int转换成字符串,
+ * 以获得更直观的类型信息, {@code Vocabulary}这个类即负责int到字符串的映射。
+ */
 public class Vocabulary {
     List<String> name;
 
