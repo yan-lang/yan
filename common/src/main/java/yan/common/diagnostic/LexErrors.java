a296d578bb4393940de392d0f7e7453303ee44cf
Zeqiang-Lai <916323301@qq.com>
2020-05-09 11:30:52 +0800

    refactor: better error message

--- common/src/main/java/yan/common/diagnostic/LexErrors.java
+++ common/src/main/java/yan/common/diagnostic/LexErrors.java
@@ -5,8 +5,21 @@ import yan.foundation.driver.log.Diagnostic;
 
 public interface LexErrors {
     default Diagnostic UnknownToken(Token token) {
-        Diagnostic d = Diagnostic.Error("unexpected token");
+        Diagnostic d = Diagnostic.Error(String.format("unexpected token '%s'", token.getText()));
         Errors.fillRangePosition(token, d, true);
         return d;
     }
+
+    default Diagnostic UnTerminatedString(Token token) {
+        Diagnostic d = Diagnostic.Error("unterminated string");
+        Errors.fillRangePosition(token, d, true);
+        return d;
+    }
+
+    default Diagnostic UnTerminatedCharacter(Token token) {
+        Diagnostic d = Diagnostic.Error("unterminated character");
+        Errors.fillRangePosition(token, d, true);
+        return d;
+    }
+
 }
