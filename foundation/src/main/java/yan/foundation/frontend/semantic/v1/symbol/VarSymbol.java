d419a41ee65ba092c2ac0145f3c4d065ed667b0a
Zeqiang-Lai <916323301@qq.com>
2020-05-19 10:41:42 +0800

    feat: add a extra field to store type of VarSymbol
    1. some passes might need to known what type of this var is

--- foundation/src/main/java/yan/foundation/frontend/semantic/v1/symbol/VarSymbol.java
+++ foundation/src/main/java/yan/foundation/frontend/semantic/v1/symbol/VarSymbol.java
@@ -6,6 +6,10 @@ import yan.foundation.frontend.semantic.v1.Type;
 public class VarSymbol extends Symbol {
     public Type type;
 
+    public enum Kind {LOCAL, FORMAL, GLOBAL}
+
+    public Kind kind;
+
     public VarSymbol(String name, Type type) {
         super(name);
         this.type = type;
